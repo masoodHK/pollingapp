4c760b8939ddc4c0e451dd97df0f90e2233e1c1e
unknown <user.name>
2018-06-24 00:22:13 +0500

    start of the Polling App with SW

---
+++ assets/main.js
@@ -0,0 +1,5 @@
+if('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('sw.js')
+        .then(reg => console.log(`Service Worker Scope: ${reg.scope}`))
+        .catch(error => console.log(error));
+}
---
+++ sw.js
@@ -0,0 +1,69 @@
+const cacheVersion = 'v2.4';
+const cacheName = `app-${cacheVersion}`;
+const files = [
+    'index.html',
+    'assets/main.js',
+    'assets/style.css'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(cacheFiles())
+    self.skipWaiting(); 
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(clients.claim(), deleteKeys());
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+      caches.open(cacheName).then(function(cache) {
+        return cache.match(event.request).then(function (response) {
+          return response || fetch(event.request).then(function(response) {
+            cache.put(event.request, response.clone());
+            return response;
+          });
+        });
+      })
+    );
+    event.waitUntil(updateCache(event.request));
+});
+function cacheFiles() {
+    return caches.open(cacheName)
+        .then(cache => {
+            cache.addAll(files);
+        })
+}
+async function cacheFilesAsync() {
+    let cache = await caches.open(cacheName);
+    cache.addAll(files);
+}
+
+function deleteKeys() {
+    return caches.keys()
+        .then(keys => Promise.all(
+            keys.map(cacheKey => {
+                if(cacheKey !== cacheName){
+                    console.log(`Deleted: ${cacheKey}`)
+                    return caches.delete(cacheKey)
+                }
+            })
+        ))
+}
+
+function updateCache(request) {
+    return caches.open(cacheName).then(function (cache) {
+        return fetch(request).then(function (response) {
+            console.log(response);
+            return cache.put(request, response);
+        });
+    });
+}
+
+async function responseAsync(request) {
+    let cache = await caches.open(cacheName)
+    let response = await cache.match(event.request)
+    let networkResponse = await fetch(request);
+    cache.put(request, networkResponse.clone())
+    return response || networkResponse;
+}
