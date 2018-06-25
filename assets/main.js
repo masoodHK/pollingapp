3d16c1f2daaf6b8e2e32b0d9a39c2ac7386e3292
Masood HK <masood_hasan@msn.com>
2018-06-25 22:06:27 +0500

    start of the firebase auth

--- assets/main.js
+++ assets/main.js
@@ -1,3 +1,13 @@
+const auth = firebase.auth();
+const database = firebase.database();
+
+function signUp() {
+    
+}
+function signIn() {
+    
+}
+
 if('serviceWorker' in navigator) {
     navigator.serviceWorker.register('sw.js')
         .then(reg => console.log(`Service Worker Scope: ${reg.scope}`))
